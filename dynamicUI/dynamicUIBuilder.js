94fed6258218ed4cfe61db987761253710d4c04a
srinidevlearn <fordevintend@gmail.com>
2022-06-05 02:30:11 +0530

    3 files modified

--- dynamicUI/dynamicUIBuilder.js
+++ dynamicUI/dynamicUIBuilder.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', ready);
 function ready() {
   // console.clear();
   console.log('DOM is ready');
-  console.log(document.querySelector('#buttonContainer'));
   loadInputSlot1();
   loadInputSlot2();
   loadOutputSlot();
@@ -42,45 +41,4 @@ function loadKeys() {
     str += `</div>`;
   }
   document.querySelector('#buttonContainer').innerHTML = str;
-}
-
-
-function createDivTag(htmlClass) {
-  let el = document.createElement('div');
-  el.classList.add(htmlClass);
-  return el;
-}
-
-function createInputTag(id, readonly = false) {
-  let el = document.createElement('input');
-  el.setAttribute('id', id);
-  if (readonly) el.setAttribute('readonly', true);
-  el.classList.add('form-control');
-  return el;
-}
-
-function createLabel(label) {
-  let el = document.createElement('label');
-  el.innerText = label;
-  return el;
-}
-
-function loadInputSlot1() {
-  document.querySelector('#inputslot1').appendChild(createLabel('Input 1'));
-  document
-    .querySelector('#inputslot1')
-    .appendChild(createInputTag('input1', true));
-}
-function loadInputSlot2() {
-  document.querySelector('#inputslot2').appendChild(createLabel('Input 2'));
-  document
-    .querySelector('#inputslot2')
-    .appendChild(createInputTag('input2', true));
-}
-function loadOutputSlot() {
-  document.querySelector('#outputslot').appendChild(createLabel('Result'));
-
-  document
-    .querySelector('#outputslot')
-    .appendChild(createInputTag('result', true));
 }
--- dynamicUI/elementDeclare.js
+++ dynamicUI/elementDeclare.js
@@ -43,3 +43,43 @@ function operandButton(key) {
   </button>
 </div>`;
 }
+
+function createDivTag(htmlClass) {
+  let el = document.createElement('div');
+  el.classList.add(htmlClass);
+  return el;
+}
+
+function createInputTag(id, readonly = false) {
+  let el = document.createElement('input');
+  el.setAttribute('id', id);
+  if (readonly) el.setAttribute('readonly', true);
+  el.classList.add('form-control');
+  return el;
+}
+
+function createLabel(label) {
+  let el = document.createElement('label');
+  el.innerText = label;
+  return el;
+}
+
+function loadInputSlot1() {
+  document.querySelector('#inputslot1').appendChild(createLabel('Input 1'));
+  document
+    .querySelector('#inputslot1')
+    .appendChild(createInputTag('input1', true));
+}
+function loadInputSlot2() {
+  document.querySelector('#inputslot2').appendChild(createLabel('Input 2'));
+  document
+    .querySelector('#inputslot2')
+    .appendChild(createInputTag('input2', true));
+}
+function loadOutputSlot() {
+  document.querySelector('#outputslot').appendChild(createLabel('Result'));
+
+  document
+    .querySelector('#outputslot')
+    .appendChild(createInputTag('result', true));
+}
